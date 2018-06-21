6e9784b893b57eb557e64b0049712629a7ed8a15
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-06-21 09:16:41 +0200

    vsdp.m: Furhter implementation.

--- @vsdp/vsdp.m
+++ @vsdp/vsdp.m
@@ -1,6 +1,8 @@
-classdef vsdp
+classdef vsdp < handle
   properties
-    At
+    n   % Condensed cone dimension `K.f + K.l + K.`
+    m   % Number of constraints
+    At  % Transposed constraint matrix `n x m`
     b
     c
     K
@@ -16,34 +18,34 @@ classdef vsdp
   end
   
   methods
-    function obj = vsdp (A, b, c, K, x, y, z)
+    function obj = vsdp (At, b, c, K, x, y, z)
       % VSDP  Conic problem data class.
       %
       %      A conic problem in primal and dual standard form is:
       %
       %         (P)  min  c'*x          (D)  max  b'*y
       %              s.t. A*x = b            s.t. z := c - A'*y
-      %                   x in K                  z in K*
+      %                   x in K                  z in K^*
       %
       %      where K is a cartesian product of the cones of free variables
-      %      (K.f), LP (K.l), SOCP (K.q), and SDP (K.s).  K* is the dual cone.
+      %      (K.f), LP (K.l), SOCP (K.q), and SDP (K.s).  K^* is the dual cone.
       %      For a theoretical introduction into verified conic programming see
       %      [Jansson2009].
       %
       %      The problem data of the block-diagonal structure:
       %
-      %         'A'  double(n,m)
+      %         'At' double(n,m)  Store transposed A, because m <<= n.
       %         'b'  double(m,1)
       %         'c'  double(n,1)
       %         'K'  struct {'f','l','q','s'}
       %
-      %      The A, b, and c may be stored in dense or sparse format and can be
-      %      interval quantities as well.
+      %      The parameters A, b, and c may be stored in dense or sparse format
+      %      and can be interval quantities.
       %
       %   obj = VSDP (A, b, c, K)
       %   obj = VSDP (    ...   , x, y, z)
       %
-      %   The class constructor accepts conic problem data in SEDUMI,
+      %   The class constructor accepts conic problem data in SeDuMi and
       %   VSDP 2006/2012 format.
       %      
       % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
@@ -51,7 +53,7 @@ classdef vsdp
       % check input
       narginchk (4, 7);
       
-      if (isempty(A) || isempty(b) || isempty(c) || isempty(K))
+      if (isempty(At) || isempty(b) || isempty(c) || isempty(K))
         error ('VSDP:VSDP', 'VSDP: empty input parameter');
       end
       if (nargin < 5)
@@ -64,86 +66,83 @@ classdef vsdp
         z = [];
       end
       
-      % If first parameter is of type cell, we assume, that the old VSDP 2006
-      % input format was used.  Therefore perform a recursive call to import
-      % that format.
-      if (iscell (A))
-        obj = vsdp.fromVSDP2006Fmt(A, b, c, K, x, y, z);
+      % If first parameter is of type cell, we assume, that the VSDP 2006 input
+      % format was used  ==>  Perform a recursive call to import that format.
+      if (iscell (At))
+        obj = vsdp.fromVSDP2006Fmt (At, b, c, K, x, y, z);
         return;
       end
       
-      % index vector to speed-up svec
-      Ivec = [];
-      
-      
-      % prepare cone structure
-      f = 0;
-      l = 0;
-      q = [];
-      s = [];
-      fields = isfield(K, {'f','l','q','s'});
-      if fields(1)
-        f = sum(K.f);
+      % Prepare cone structure and determine condensed cone dimension.
+      obj.n = 0;
+      obj.K.f = 0;
+      obj.K.l = 0;
+      obj.K.q = [];
+      obj.K.s = [];
+      if (isfield (K, 'f'))
+        obj.K.f = sum(K.f);
+        obj.n = obj.n + obj.K.f;
       end
-      if fields(2)
-        l = sum(K.l);
+      if (isfield (K, 'l'))
+        obj.K.l = sum(K.l);
+        obj.n = obj.n + obj.K.l;
       end
-      if fields(3)
-        q = K.q(K.q > 0);
+      if (isfield (K, 'q'))
+        obj.K.q = K.q(K.q > 0);
+        obj.n = obj.n + sum (obj.K.q);
       end
-      if fields(4)
-        s = K.s(K.s > 0);
+      if (isfield (K, 's'))
+        obj.K.s = K.s(K.s > 0);
+        obj.n = obj.n + (sum(obj.K.s .* (obj.K.s + 1)) / 2);
       end
-      obj.K = struct('f', f, 'l', l, 'q', q(:), 's', s(:));
-      % appropriate dimensions
-      dim3 = f + l + sum(q) + (sum(s .* (s + 1)) / 2);
-      
       
       % prepare interval input for b
-      if (~isnumeric(b) && ~isa(b, 'intval'))
-        error('VSDP:IMPORT_VSDP','cannot import dual objective "b"');
+      if (~isfloat (b) && ~isa (b, 'intval'))
+        error ('VSDP:VSDP','cannot import dual objective "b"');
       end
       obj.b = b(:);
       m = length(b);
       
       % prepare interval input for A
-      if (~isnumeric(A) && ~isa(A, 'intval'))
+      if (~isfloat (At) && ~isa(At, 'intval'))
         error('VSDP:IMPORT_VSDP','cannot import coefficient matrix "A"');
       end
+      % index vector to speed-up svec
+      Ivec = [];
       % compact vectorized format
-      if (any(size(A) - [dim3, m]) && any(size(A) - [m, dim3]))
+      if (any(size(At) - [n, m]) && any(size(At) - [m, n]))
         imported_fmt = 'SEDUMI';
-        [A,Ivec] = vsvec(A,K,1,0,Ivec);
+        [At,Ivec] = vsvec(At,K.s,1,0,Ivec);
       end
       % transposed format
-      if (size(A,1) == m)
-        A = A';
+      if (size(At,1) == m)
+        At = At';
       end
       % check size of A
-      if any(size(A) ~= [dim3, m])
+      if any(size(At) ~= [n, m])
         error('VSDP:IMPORT_VSDP','wrong dimension of coefficient matrix "A"');
       end
-      obj.At = A;
+      obj.At = At;
       
       % prepare interval input for c
-      if (~isnumeric(c) && ~isa(c, 'intval'))
+      if (~isfloat(c) && ~isa(c, 'intval'))
         error('VSDP:IMPORT_VSDP','cannot import primal objective "c"');
       end
       c = c(:);
       % compact vectorized format
-      if (length(c) ~= dim3)
+      if (length(c) ~= n)
         [c,Ivec] = vsvec(c,K,1,0,Ivec);
       end
       obj.c = c;
       
       % prepare x
       if (~isempty(x))
-        if (~isnumeric(x))
+        if (~isfloat(x))
           error('VSDP:IMPORT_VSDP','primal solution vector "x" has to be numeric');
         end
         x = x(:);
         % compact vectorized format, mu=2
-        if (length(x) ~= dim3)
+        if (length(x) ~= n)
           [x,Ivec] = vsvec(x,K,1,0,Ivec);  % Ivec can only be used with mu=1
           x = sscale(x,K,2);
         end
@@ -153,7 +152,7 @@ classdef vsdp
       % prepare y
       if (~isempty(y))
         y = y(:);
-        if (~isnumeric(y) || (length(y) ~= m))
+        if (~isfloat(y) || (length(y) ~= m))
           error('VSDP:IMPORT_VSDP','cannot import dual solution vector "y"');
         end
       end
@@ -161,12 +160,12 @@ classdef vsdp
       
       % prepare z
       if (~isempty(z))
-        if (~isnumeric(z))
+        if (~isfloat(z))
           error('VSDP:IMPORT_VSDP','cannot import dual solution "z"');
         end
         z = z(:);
         % compact vectorized format
-        if (size(z,1) ~= dim3)
+        if (size(z,1) ~= n)
           z = vsvec(z,K,1,0,Ivec);
         end
       end
