72d9cea23f2a53a7519739e6d05dba1a0c0f7173
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-06-28 15:00:04 +0200

    from_sdpt3_fmt.m: Overhaul function.

--- @vsdp/from_sdpt3_fmt.m
+++ @vsdp/from_sdpt3_fmt.m
@@ -1,140 +1,118 @@
-function [K,A,c,x,z] = from_sdpt3_fmt(blk,A,c,x,z)
-% SDPT2VSDP  Convert data in SDPT3 input format to VSDP 2012 format.
+function obj = from_sdpt3_fmt (blk, At, b, c, x0, y0, z0)
+% FROM_SDPT3_FMT  Import conic problem from a SDPT3 format.
 %
-%   [K,A,c,x,z] = sdpt2vsdp(blk,At,c,x,z)
+%   obj = vsdp.FROM_SDPT3_FMT (blk, At, b, c, x0, y0, z0);
 %
-% 
+%   The problem data of the SDPT3-4.0 is:
 %
-% Input: except for blk all input parameter are optional
-% blk: a cell array describing the block diagonal structure of SQL data
-% At: a cell array with At{1} = A(1:dimf,:), At{2} = A(dimf+1:dimf+diml,:)
-%     and the same for socp cone, and At{i} = [svec(Ai1) ... svec(Aim)]
-%     for positive semidefinite cone constraint matrices, where Aij is the
-%     matrix of j-th block and i-th constraint.
-% c: a cell array of matrices of dimensions given by K
-%       -> correspond to primal objective function
-% x: a cell array of matrices of dimensions given by K
-%       -> approximate primal optimal solution
-% z: a cell array of matrices of dimensions given by K
+%      'n'  Number of cones (semidefinite 's', quadratic 'q', linear'l',
+%                            unrestricted (free variables) 'u')
+%      'blk'  cell(n,2)
+%      'At'   cell(n,1)  Each cell  double(n_j,m)
+%      'c'    cell(n,1)
+%      'b'  double(m,1)  Identical to current VSDP format.
 %
-% Output:
-% K: a structure with following fields
-%     - K.f stores the number of free variables
-%     - K.l is the number of nonnegative components
-%     - K.q lists the lengths of socp blocks
-%     - K.s lists the dimensions of semidefinite blocks
-% A: a nA3 x M Matrix,
-%     whereas nA = dimf+diml+dimq+dims
-%     dimf: number of free variables: dimf = sum(K.f>0)
-%     diml: number of nonnegative variables: diml = sum(K.l>0)
-%     dimq: sum of all socp variables: dimq = sum_i(K.q(i))
-%     dims3: sum of sdp variables: dims3 = sum_i(K.s(i)*(K.s(i)+1)/2)
-% c: an nA3 x 1 vector  - svec(mu=1)
-% x: an nA3 x 1 vector   -> approximate optimal solution  - svec(mu=2)
-% z: an nA3 x 1 vector   -> approximate LMI dual vector  - svec(mu=1)
+%   In case of a semidefinite cone j, the matrix At{j} has the condensed format,
+%   but is scaled with factor 'sqrt(2)'.
+%
+%   The optional initial guess format is:
+%
+%      'x0'    cell(n,1)
+%      'y0'  double(m,1)  Identical to current VSDP format.
+%      'z0'    cell(n,1)
+%
+%   The following example with block diagonal structure was taken from [2] with
+%   vectorization, described in [1].
+%
+%   Example:
+%
+%      C{1}    = [ 0 0 0 0;
+%                  0 0 0 0;
+%                  0 0 1 2;
+%                  0 0 2 1];
+%      blk{1,1} = 's'; blk{1,2} = [2 2];
+%      At{1} = [ ...
+%         svec(blk, [ 1 0 0 0;
+%                     0 1 0 0;
+%                     0 0 0 0;
+%                     0 0 0 0]), ...
+%         svec(blk, [ 1 0 0 0;
+%                     0 0 0 0;
+%                     0 0 1 0;
+%                     0 0 0 0]), ...
+%         svec(blk, [ 0 1 0 0;
+%                     1 0 0 0;
+%                     0 0 0 1;
+%                     0 0 1 0]), ...
+%         svec(blk, [ 0 0 0 0;
+%                     0 1 0 0;
+%                     0 0 0 0;
+%                     0 0 0 1])];
+%       b = [1; 1; 1; 1];
+%       obj = vsdp.FROM_SDPT3_FMT (blk, At, b, C);
 %
 %
-% Note that the right hand side of the linear constraints (b) and the
-% dual optimal solution vector (y) have the same format in VSDP and SDPT3.
+% For more information on the SDPT3-4.0 format, see:
+%
+%   [1] http://www.math.nus.edu.sg/~mattohkc/sdpt3/guide4-0-draft.pdf
+%   [2] http://www.math.nus.edu.sg/~mattohkc/sdpt3/sdpexample.html
 %
 
-% Copyright 2004-2012 Christian Jansson (jansson@tuhh.de)
+% Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
 
-% check input
-if nargin<1 || isempty(blk)
-  error('VSDP:SDPT2VSDP','not enough input parameter');
-elseif nargin>1 && iscell(A) && all(min(size(A))~=[0 1])
-  error('VSDP:SDPT2VSDP',['this is an old SDPT3 input format '...
-    'not supported by this function']);
-elseif size(blk,2)>2 && any([blk{:,3}])
-  error('VSDP:SDPT2VSDP','unsupported SDPT3 low rank structure input');
+narginchk(4, 7);
+
+% Convert cone structure.
+if (isempty (blk))
+  error('VSDP:from_sdpt3_fmt:noConeStructure', ...
+    'from_sdpt3_fmt: Cannot convert cone structure.');
+end
+if (size (blk, 2) > 2)
+  error('VSDP:from_sdpt3_fmt:lowRankStructureUnsupported', ...
+    'from_sdpt3_fmt: VSDP does not support low rank structures.');
 end
 
+% Cell indices of respective cones.
+idx_f = ([blk{:, 1}] == 'u');
+idx_l = ([blk{:, 1}] == 'l');
+idx_q = ([blk{:, 1}] == 'q');
+idx_s = ([blk{:, 1}] == 's');
 
-% permutation index for cells if necessary
+K.f = sum ([blk{idx_f, 2}]);
+K.l = sum ([blk{idx_l, 2}]);
+K.q = [blk{idx_q, 2}];
+K.s = [blk{idx_s, 2}];
 
-% declare blk-vector
-blkvec = cat(1,blk{:,1});
-
-% indices to sort variables  f -> l -> q -> s
-indf = find(blkvec=='u');
-indl = find(blkvec=='l');
-indq = find(blkvec=='q');
-inds = find(blkvec=='s');
-
-ind = [indf; indl; indq; inds];
-
-if size(ind,1)~=size(blk,1)
-  error('VSDP:SDPT2VSDP','unsupported cell format or empty cells');
+% Ensure cone order.
+sort_idx = [find(idx_f), find(idx_l), find(idx_q), find(idx_s)];
+if (length (sort_idx) ~= size (blk, 1))
+  error('VSDP:from_sdpt3_fmt:unsupportedCones', ...
+    'from_sdpt3_fmt: unsupported ''blk'' cell format or empty cells');
 end
+c  =  c(sort_idx);
+At = At(sort_idx);
 
+% Convert cells to matrices.
+c  = vsdp.cell2mat (c(:));
+At = cell2mat (At(:));  % No vectorization needed, use native 'cell2mat'.
 
-% create K
-K = struct('f',sum([blk{indf,2}]),'l',sum([blk{indl,2}]),...
-  'q',[blk{indq,2}]','s',[blk{inds,2}]');
-
-% what has to be processed
-doA = nargin>1 && iscell(A) && ~isempty(A{1}) && nargout>1;
-doC = nargin>2 && iscell(c) && ~isempty(c{1}) && nargout>2;
-doX = nargin>3 && iscell(x) && ~isempty(x{1}) && nargout>3;
-doZ = nargin>4 && iscell(z) && ~isempty(z{1}) && nargout>4;
-
-
-% convert c,x,z
-if doC || doX || doZ
-  % vectorize sdp blocks
-  for i = 1:length(inds)
-    j = inds(i);  % index of current sdp block group
-    js = blk{j,2}(:);  % index set for sdp blocks of j-th sdp group
-    if length(js)==1
-      blkI = triu(true(js));
-    else  % extract block diagonal
-      % block diagonal index computation to avoid additional for-loop
-      blkI = ones((js'*(js+1))/2,1);
-      ofI = ones(sum(js),1);
-      ofV = -ofI;
-      js(end) = [];
-      ofI(cumsum([2; js])) = [0; 1-js];
-      ofV(cumsum([1; js])) = [1; js-1];
-      ofV(2) = length(ofV) - (js(1)>1);
-      blkI(cumsum(cumsum(ofI(1:length(ofV))))) = cumsum(ofV);
-      blkI = cumsum(blkI);
-    end
-    if doC
-      c{j} = c{j}(blkI);
-    end
-    if doX
-      x{j} = x{j}(blkI);
-    end
-    if doZ
-      z{j} = z{j}(blkI);
-    end
-  end
-end
-
-% cell to mat
-if doC
-  c = cat(1,c{ind});
+% Treat optional paramter.
+if (nargin >= 5)
+  x0 = x0(sort_idx);
+  x0 = vsdp.cell2mat (x0(:));
 else
-  c = [];
+  x0 = [];
 end
-if doX
-  x = sscale(cat(1,x{ind}),K,2);
-else
-  x = [];
+if (nargin < 6)
+  y0 = [];
 end
-if doZ
-  z = cat(1,z{ind});
+if (nargin == 7)
+  z0 = z0(sort_idx);
+  z0 = vsdp.cell2mat (z0(:));
 else
-  z = [];
+  z0 = [];
 end
 
-
-% convert A
-if doA
-  A = sscale(cat(1,A{ind}),K,sqrt(0.5));
-else
-  A = [];
-end
+obj = vsdp (At, b, c, K, x0, y0, z0);
 
 end
