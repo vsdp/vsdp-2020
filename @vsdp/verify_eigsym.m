b57987285e0ccb7452086b3106481501cc671a57
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-07-26 14:44:27 +0200

    @vsdp.verify_eigsym(): Improve documentation and function.

--- @vsdp/verify_eigsym.m
+++ @vsdp/verify_eigsym.m
@@ -1,10 +1,14 @@
 function E = verify_eigsym (A)
-% VEIGSYM  Verified enclosure for all eigenvalues of a symmetric matrix.
+% VERIFY_EIGSYM  Verified enclosure for all eigenvalues of a symmetric matrix.
 %
-%   E = VEIGSYM(A) a verified enclosure for all eigenvalues of matrix 'A'
+%   E = VERIFY_EIGSYM(A) a verified enclosure for all eigenvalues of matrix 'A'
 %      in form of an interval vector 'E' is computed.  The matrix 'A' must
 %      be a full or sparse symmetric matrix.  'A' is allowed to be a real or
 %      interval quantity.
+%
+%   For more theoretical background, see:
+%
+%     https://vsdp.github.io/references.html#Jansson2007 (p. 19)
 %
 %   Example:
 %
@@ -12,21 +16,21 @@ function E = verify_eigsym (A)
 %            2 1 4;
 %            3 4 5];
 %       A = midrad  (A, 0.01*ones(3));
-%       E = veigsym (A);
+%       E = vsdp.verify_eigsym (A);
 %
 %   See also vsdplow, vsdpup, vsdpinfeas.
 
 % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
 
-if ~min(min( mid(A) == mid(A)'))
-  error('VSDP:VEIGSYM', 'matrix must be symmetric')
+if (~issymmetric (mid (A)))
+  error ('VSDP:verify_eigsym:notSymmetric', ...
+    'verify_eigsym: Matrix must be symmetric.')
 end
 
-% Main routine using Weyl's Perturbation Theorem
-% see Lecture Notes on Optimization with Result Verification
+% Make use of Weyl's Perturbation Theorem.
 [V, D] = eig (full (mid (A)));
-E = A - V * intval(D) * V';
-r = abss(norm(E,inf));
-E = midrad(diag(D),r);
+E = A - V * intval (D) * V';
+r = mag (norm (E, inf));
+E = midrad (diag (D), r);
 
 end
