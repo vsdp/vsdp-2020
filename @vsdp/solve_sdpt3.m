cf0d1941457c6f7085722d3fd4d6b18a983d08fd
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-07-28 13:09:50 +0200

    @vsdp.solve_sdpt3(): Minor changes and adaptions.

--- @vsdp/solve_sdpt3.m
+++ @vsdp/solve_sdpt3.m
@@ -11,25 +11,21 @@ function obj = solve_sdpt3 (obj)
 
 % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
 
-% In case of interval data solve midpoint problem.
-A = mid (obj.At);
-b = mid (obj.b);
-c = mid (obj.c);
-
 % Should initial solution guess be taken into account?
 if ((obj.options.USE_STARTING_POINT) && (~isempty (obj.solution)))
   [x0, y0, z0] = deal (obj.solution.x, obj.solution.y, obj.solution.z);
 else
   [x0, y0, z0] = deal ([], [], []);
 end
-% Clear previous solutions or initial points.
-obj.solution = [];
 
+% Should special solver options be taken into account?
 if (~isempty (obj.options.SOLVER_OPTIONS))
   OPTIONS = obj.options.SOLVER_OPTIONS;
 else
   OPTIONS = [];
 end
+
+% Adapt output verbosity.
 if (~obj.options.VERBOSE_OUTPUT)
   if (exist ('sqlpmain.m', 'file') == 2) % if SDPT3-4.0
     OPTIONS.printlevel = 0; % default: 3
@@ -37,23 +33,28 @@ if (~obj.options.VERBOSE_OUTPUT)
     OPTIONS.printyes = 0;   % default: 1
   end
 end
+
+% Prepare data for solver.
+[At, b, c] = deal (mid (obj.At), mid (obj.b), mid (obj.c));
 warning ('off', 'VSDP:svec:justScale');
-A = mat2cell (vsdp.svec (obj, A, sqrt(2)), obj.K.dims, obj.m);
+A = mat2cell (vsdp.svec (obj, At, sqrt(2)), obj.K.dims, obj.m);
 warning ('on', 'VSDP:svec:justScale');
 c = mat2cell (c, obj.K.dims, 1);
 for i = 1:length(obj.K.s)
   c{i} = vsdp.smat ([], c{i}, 1);
 end
 blk = obj.K.blk;
-% SDPT3 terminology is "unconstrained" instead of "free" variables.
+% In SDPT3 terminology "unconstrained" are "free" variables.
 if (blk{1,1} == 'f')
   blk{1,1} = 'u';
 end
 
 % Call solver.
+tic;
 [~, x, y, z, INFO] = sqlp (blk, A, c, b, OPTIONS, x0, y0, z0);
+elapsed_time = toc;
 
-% Store results.
+% Store solution.
 x = vsdp.svec (obj, vsdp.cell2mat (x(:)), 2);
 z = vsdp.svec (obj, vsdp.cell2mat (z(:)), 1);
 f_objective = [obj.c'*x; obj.b'*y];
@@ -62,6 +63,6 @@ if (isstruct (INFO))
 else
   info = INFO(1);  % SDPT3-3.x output
 end
-obj.add_solution(x, y, z, f_objective, obj.options.SOLVER, info)
+obj.add_solution(x, y, z, f_objective, obj.options.SOLVER, info, elapsed_time);
 
 end
