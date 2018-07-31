73facc25118a24d91c5051a0e6affde465bc84fe
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-07-31 14:11:37 +0200

    @vsdp_solution: Generalize class.

--- vsdp_solution.m
+++ vsdp_solution.m
@@ -11,7 +11,7 @@ classdef vsdp_solution < handle
     %   - 'Certificate primal infeasibility',
     %   - 'Certificate dual infeasibility'
     %
-    type
+    sol_type
     x   % Primal solution quantity.
     y   % Dual   solution quantity.
     z   % Slack  or bound quantity.
@@ -33,54 +33,60 @@ classdef vsdp_solution < handle
   end
   
   methods
-    function obj = vsdp_solution (type, x, y, z, f_objective, solver_info)
+    function obj = vsdp_solution (sol_type, x, y, z, f_objective, solver_info)
       % VSDP_SOLUTION  Container class for VSDP solutions.
       %
-      %   sol = vsdp_solution(type, x, y, z)  Create a solution object.
+      %   sol = vsdp_solution(sol_type, x, y, z)  Create a solution object.
       %
       %   sol = vsdp_solution(..., f_objective, solver_info)
       %         Create a solution object.
       %
       %   Example:
       %
-      %      type = 'Initial guess'
+      %      sol_type = 'Initial guess';
       %      x = [1 2 3]';  % Primal solution.
       %      y = [4 5]';    % Dual solution.
       %      z = [];
-      %      sol = vsdp_approx_solution (type, x, y, z)
+      %      sol = vsdp_solution (sol_type, x, y, z)
       %
       %
-      %   See also vsdp.vsdp.
+      %   See also vsdp.
       
       % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
       
       narginchk (4, 6);
       
-      obj.type = validatestring (type, {'Initial guess', ...
+      obj.sol_type = validatestring (sol_type, {'Initial guess', ...
         'Approximate solution', 'Rigorous lower bound', ...
         'Rigorous upper bound', 'Certificate primal infeasibility', ...
         'Certificate dual infeasibility'});
       
       if (~isempty (x))
-        validateattributes (x, {'double'}, {'vector'});
+        validateattributes (x, {'double', 'intval'}, {'vector'});
         obj.x = x(:);
       else
         obj.x = [];
       end
       if (~isempty (y))
-        validateattributes (y, {'double'}, {'vector'});
+        validateattributes (y, {'double', 'intval'}, {'vector'});
         obj.y = y(:);
       else
         obj.y = [];
       end
       if (~isempty (z))
-        validateattributes (z, {'double'}, {'vector'});
+        validateattributes (z, {'double', 'intval'}, {'vector'});
         obj.z = z(:);
       else
         obj.z = [];
       end
+      if (~isempty (obj.x) && ~isempty (obj.z))
+        if (length (obj.x) ~= length (obj.z))
+          error ('VSDP:vsdp_solution:sizeMissmatch', ...
+            'vsdp_solution: The lengths of ''x'' and ''z'' must agree.');
+        end
+      end
       if (nargin > 4)
-        validateattributes (f_objective, {'double'}, {'size', [2, 1]});
+        validateattributes (f_objective(:), {'double'}, {'size', [2, 1]});
         obj.f_objective = full (f_objective(:));
       else
         obj.f_objective = nan (2, 1);
@@ -94,8 +100,8 @@ classdef vsdp_solution < handle
           obj.solver_info.name = 'Unknown';
         end
         if (isfield (obj.solver_info, 'elapsed_time'))
-          obj.solver_info.elapsed_time = validateattributes (elapsed_time, ...
-            {'double'}, {'scalar'});
+          validateattributes (obj.solver_info.elapsed_time, {'double'}, ...
+            {'scalar'});
         else
           obj.solver_info.elapsed_time = nan;
         end
@@ -115,7 +121,7 @@ classdef vsdp_solution < handle
     function varargout = size (obj, dim)
       % SIZE  Size of the solution instance.
       m = length (obj.y);
-      n = max (length (obj.x), length (obj.z));
+      n = length (obj.x);
       if (nargin == 1)
         if (nargout < 2)
           varargout = {[m, n]};
@@ -138,29 +144,39 @@ classdef vsdp_solution < handle
     
     function disp (obj)
       % DISP  Visualize the solution.
-      fprintf ('  [x] %s:\n', obj.type);
-      switch (obj.type)
+      if (~isempty (obj.solver_info))
+        if (isfield (obj.solver_info, 'iter'))
+          iter_str = sprintf (', %d iterations', obj.solver_info.iter);
+        else
+          iter_str = '';
+        end
+        fprintf ('      Solver ''%s'': %s, %.1f seconds%s.\n\n', ...
+          obj.solver_info.name, obj.solver_info.termination, ...
+          obj.solver_info.elapsed_time, iter_str);
+      end
+      switch (obj.sol_type)
         case 'Initial guess'
           obj.mem_info ();
         case 'Approximate solution'
-          if (~isempty (obj.solver_info))
-            fprintf ('      ');
-            fprintf ('Solver ''%s'': %s termination after %.1f seconds.\n', ...
-              obj.solver_info.name, obj.solver_info.termination, ...
-              obj.solver_info.elapsed_time);
-          end
           if (~all (isnan (obj.f_objective)))
-            fprintf ('\n      ');
-            fprintf ('c''*x = %.15e',     obj.f_objective(1));
-            fprintf ('\n      ');
-            fprintf ('b''*y = %.15e\n\n', obj.f_objective(2));
+            fprintf ('        c''*x = %.15e\n', obj.f_objective(1));
+            fprintf ('        b''*y = %.15e\n', obj.f_objective(2));
           else
             obj.mem_info ();
           end
+          fprintf ('\n');
         case 'Rigorous lower bound'
+          fprintf ('          fL = %.15e\n',   obj.f_objective(1));
+          fprintf ('\n');
         case 'Rigorous upper bound'
+          obj.mem_info ();
+          fprintf ('\n');
         case 'Certificate primal infeasibility'
+          obj.mem_info ();
+          fprintf ('\n');
         case 'Certificate dual infeasibility'
+          obj.mem_info ();
+          fprintf ('\n');
         otherwise
           error ('VSDP_SOLUTION:disp:unknownType', ...
             'disp: Unknown solution type.');
