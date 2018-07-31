8b2b38998576ede5ee8660a13a40fe4e584eb660
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-07-31 14:12:22 +0200

    @vsdp: Adapt for new solution storage.  Relocate help string.

--- @vsdp/vsdp.m
+++ @vsdp/vsdp.m
@@ -1,4 +1,68 @@
 classdef vsdp < handle
+  % VSDP  [V]erified [S]emi[D]efinite conic [P]rogramming.
+  %
+  %      A conic problem in primal and dual standard form is:
+  %
+  %         (P)  min   c'*x          (D)  max  b'*y
+  %              s.t. At'*x = b           s.t. z := c - At*y
+  %                       x in K               z in K^*
+  %
+  %      where K is a cartesian product of the cones of free variables
+  %      (K.f), LP (K.l), SOCP (K.q), and SDP (K.s).  K^* is the dual cone.
+  %      For a theoretical introduction into verified conic programming see
+  %
+  %         https://vsdp.github.io/references#Jansson2009
+  %
+  %      The problem data of the block-diagonal structure:
+  %
+  %         'At' (n x m)  Stored transposed A, usually m <<= n.
+  %         'b'  (m x 1)
+  %         'c'  (n x 1)
+  %         'K'  struct{'f','l','q','s'}
+  %
+  %      The parameters A, b, and c may be stored in dense or sparse format
+  %      and can be interval quantities.
+  %
+  %      The VSDP constructor can be called directly
+  %
+  %         obj2 = VSDP (obj1)                       % Copy Problem data.
+  %         obj  = VSDP (At,  b,  c, K)              % 2012 Format
+  %         obj  = VSDP (    ...      , x0, y0, z0)  % 2012 Format
+  %         obj  = VSDP (blk, At, C, b, X0, y0, Z0)  % 2006 Format
+  %
+  %      or the problem data can be imported by calling one of the methods:
+  %
+  %         obj = VSDP.from_mps_file     (...)
+  %         obj = VSDP.from_sdpa_file    (...)
+  %         obj = VSDP.from_sdpa_fmt     (...)
+  %         obj = VSDP.from_sdpt3_fmt    (...)
+  %         obj = VSDP.from_2006_fmt     (...)
+  %         obj = VSDP.from_lp_solve_fmt (...)
+  %
+  %   To use an initial guess (x0,y0,z0) type:
+  %
+  %      obj.add_solution ('initial guess', x0, y0, z0);
+  %      obj.options.USE_INITIAL_GUESS = true;
+  %
+  %   Example:
+  %
+  %       A1 = [0 1;
+  %             1 0];
+  %       A2 = [1 1;
+  %             1 1];
+  %       C =  [1 0;
+  %             0 1];
+  %       K.s = 2;
+  %       At = [A1(:), A2(:)];  % Vectorize data
+  %       c  = C(:);
+  %       b  = [1; 2.0001];
+  %       obj = vsdp(At, b, c, K).solve()
+  %
+  %   See also  VSDP.from_lp_solve_fmt, VSDP.from_sdpa_fmt,
+  %             VSDP.from_sdpt3_fmt,    VSDP.from_sdpa_file,
+  %             VSDP.from_mps_file,     VSDP.from_2006_fmt.
+  
+  % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
   
   properties (GetAccess = public, SetAccess = protected)
     % Condensed cone dimension.
@@ -17,7 +81,10 @@ classdef vsdp < handle
     cache_memory % Memory for caching expensive computation results.
     options      % Options for this problem instance.
     pertubation  % Pertubation parameter.
-    solution     % Approximate solution or initial guess.
+    % Solutions for the conic problem.
+    %
+    % See also vsdp_solution.
+    solutions
   end
   
   methods (Static)
@@ -41,71 +108,10 @@ classdef vsdp < handle
   
   methods
     function obj = vsdp (varargin)
-      % VSDP  Conic problem data class.
+      % VSDP  Constructor for conic problem data class.
       %
-      %      A conic problem in primal and dual standard form is:
-      %
-      %         (P)  min   c'*x          (D)  max  b'*y
-      %              s.t. At'*x = b           s.t. z := c - At*y
-      %                       x in K               z in K^*
-      %
-      %      where K is a cartesian product of the cones of free variables
-      %      (K.f), LP (K.l), SOCP (K.q), and SDP (K.s).  K^* is the dual cone.
-      %      For a theoretical introduction into verified conic programming see
-      %
-      %         https://vsdp.github.io/references#Jansson2009
-      %
-      %      The problem data of the block-diagonal structure:
-      %
-      %         'At' double(n,m)  Store transposed A, usually m <<= n.
-      %         'b'  double(m,1)
-      %         'c'  double(n,1)
-      %         'K'  struct{'f','l','q','s'}
-      %
-      %      The parameters A, b, and c may be stored in dense or sparse format
-      %      and can be interval quantities.
-      %
-      %      The VSDP constructor can be called directly
-      %
-      %         obj2 = VSDP (obj1)                       % Copy Problem data.
-      %         obj  = VSDP (At,  b,  c, K)              % 2012 Format
-      %         obj  = VSDP (    ...      , x0, y0, z0)  % 2012 Format
-      %         obj  = VSDP (blk, At, C, b, X0, y0, Z0)  % 2006 Format
-      %
-      %      or the problem data can be imported by calling one of the methods:
-      %
-      %         obj = VSDP.from_mps_file     (...)
-      %         obj = VSDP.from_sdpa_file    (...)
-      %         obj = VSDP.from_sdpa_fmt     (...)
-      %         obj = VSDP.from_sdpt3_fmt    (...)
-      %         obj = VSDP.from_2006_fmt     (...)
-      %         obj = VSDP.from_lp_solve_fmt (...)
-      %
-      %   To use an initial guess (x0,y0,z0) type:
-      %
-      %      obj.add_solution (x0, y0, z0);
-      %      obj.options.USE_STARTING_POINT = true;
-      %
-      %   Example:
-      %
-      %       A1 = [0 1;
-      %             1 0];
-      %       A2 = [1 1;
-      %             1 1];
-      %       C =  [1 0;
-      %             0 1];
-      %       K.s = 2;
-      %       At = [A1(:), A2(:)];  % Vectorize data
-      %       c  = C(:);
-      %       b  = [1; 2.0001];
-      %       obj = vsdp(At, b, c, K).solve()
-      %
-      %   See also  VSDP.from_lp_solve_fmt, VSDP.from_sdpa_fmt,
-      %             VSDP.from_sdpt3_fmt,    VSDP.from_sdpa_file,
-      %             VSDP.from_mps_file,     VSDP.from_2006_fmt.
-      
-      % Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
-      
+      %   See also vsdp.
+     
       narginchk (1, 7);
       
       switch (nargin)
@@ -131,13 +137,20 @@ classdef vsdp < handle
           obj.options = vsdp_options ();
           obj.pertubation.b = [];
           obj.pertubation.c = [];
-          obj.solution = [];
+          obj.solutions = containers.Map({ ...
+            'Initial guess', ...
+            'Approximate solution', ...
+            'Rigorous lower bound', ...
+            'Rigorous upper bound', ...
+            'Certificate primal infeasibility', ...
+            'Certificate dual infeasibility'}, ...
+            {[], [], [], [], [], []});
           obj = obj.validate ();
         case {5, 6, 7}
           % First create VSDP problem instance.
           obj = vsdp (varargin{1:4});
           % Then add initial solution guess.
-          obj.add_solution (varargin{5:end});
+          obj.add_solution ('Initial guess', varargin{5:end});
         otherwise
           error ('VSDP:vsdp:badNumberOfArguments', ...
             ['vsdp: Bad number of arguments, type ', ...
