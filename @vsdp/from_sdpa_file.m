97000856eeb7a532148dcc1752764743fdf53ae8
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-09-10 16:16:01 +0200

    @vsdp.from_sdpa_file(): Improve error message IDs.

--- @vsdp/from_sdpa_file.m
+++ @vsdp/from_sdpa_file.m
@@ -54,13 +54,13 @@ switch (fext)
     % any i>j ?
     idx = find(i>j);
     if ~isempty(idx)
-      warning ('VSDP:from_sdpa_file:', ...
-	    'Lower triangular elements will be ignored.');
+      warning ('VSDP:from_sdpa_file:readError', ...
+        'Lower triangular elements will be ignored.');
       col(idx) = [];
-	  blk(idx) = [];
-	  i(idx) = [];
-	  j(idx) = [];
-	  data(idx) = [];
+      blk(idx) = [];
+      i(idx) = [];
+      j(idx) = [];
+      data(idx) = [];
     end
     
     % block information
@@ -114,14 +114,14 @@ switch (fext)
     [c,cnt] = fscanf(fid,'%*[^0-9+-]%f',[dim 1]);
     if cnt~=dim
       fclose(fid);
-      error('VSDP:from_sdpa_file:', ...
-	    'from_sdpa_file: Could not read SDPA data');
+      error('VSDP:from_sdpa_file:readError', ...
+        'from_sdpa_file: Could not read SDPA data');
     end
     [A,cnt] = fscanf(fid,'%*[^0-9+-]%f',[dim m]);
     if cnt~=dim*m
       fclose(fid);
-      error('VSDP:from_sdpa_file:', ...
-	    'from_sdpa_file: Could not read SDPA data');
+      error('VSDP:from_sdpa_file:readError', ...
+        'from_sdpa_file: Could not read SDPA data');
     end
     fclose(fid);
     
@@ -147,9 +147,9 @@ switch (fext)
     
     % Cone information from blksize
     if nargin < 2 || isempty(blksize)
-      error ('VSDP:from_sdpa_file:', ...
-	    ['from_sdpa_file: Block sizes "blksize" has to be set to read ', ...
-		'initial data']);
+      error ('VSDP:from_sdpa_file:readError', ...
+        ['from_sdpa_file: Block sizes "blksize" has to be set to read ', ...
+        'initial data']);
     end
     
     % dual initial vector y0
@@ -160,22 +160,22 @@ switch (fext)
     [data,cnt] = fscanf(fid,'%*[^0-9+-]%d %d %d %d %lf',[5 inf]);
     fclose(fid);
     if cnt==0 || mod(cnt,5)~=0
-      error ('VSDP:from_sdpa_file:', ...
-	    'from_sdpa_file: Could not read SDPA data');
+      error ('VSDP:from_sdpa_file:readError', ...
+        'from_sdpa_file: Could not read SDPA data');
     end
     [col, blk, i, j, data] = deal(data(1,:), data(2,:), data(3,:), ...
-	  data(4,:), data(5,:));
+      data(4,:), data(5,:));
     
     % Any i > j?
     idx = i>j;
     if any(idx)
-      warning ('VSDP:from_sdpa_file:', ...
-	    'from_sdpa_file: Lower triangular elements will be ignored.');
+      warning ('VSDP:from_sdpa_file:readError', ...
+        'from_sdpa_file: Lower triangular elements will be ignored.');
       col(idx) = [];
-	  blk(idx) = [];
-	  i(idx) = [];
-	  j(idx) = [];
-	  data(idx) = [];
+      blk(idx) = [];
+      i(idx) = [];
+      j(idx) = [];
+      data(idx) = [];
     end
     
     % block information
@@ -205,8 +205,8 @@ switch (fext)
     
     % cone information from blksize
     if (nargin<2 || isempty(blksize))
-      error ('VSDP:from_sdpa_file:', ...
-	    'block sizes "blksize" has to be set to read initial data');
+      error ('VSDP:from_sdpa_file:readError', ...
+        'block sizes "blksize" has to be set to read initial data');
     end
     
     % dual initial vector y0
@@ -227,12 +227,14 @@ switch (fext)
     [c,cnt] = fscanf(fid,'%*[^0-9+-]%f',[dim 1]);
     if (cnt~=dim)
       fclose(fid);
-      error('VSDP:from_sdpa_file:', ..., ...,'Could not read SDPA data');
+      error ('VSDP:from_sdpa_file:readError', ...
+        'Could not read SDPA data');
     end
     [A,cnt] = fscanf(fid,'%*[^0-9+-]%f',[dim 1]);
     if (cnt~=dim)
       fclose(fid);
-      error('VSDP:from_sdpa_file:', ..., ...,'Could not read SDPA data');
+      error ('VSDP:from_sdpa_file:readError', ...
+        'Could not read SDPA data');
     end
     fclose(fid);
     
