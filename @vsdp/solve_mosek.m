5316dbfb4f6633b19bc51331b2a5b90d7898b0b0
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-09-12 14:32:57 +0200

    @vsdp.solve_mosek(): Support for SDP cones only.

--- @vsdp/solve_mosek.m
+++ @vsdp/solve_mosek.m
@@ -70,9 +70,9 @@ solver_info.elapsed_time = toc;
 if (isscalar(r) && isnumeric(r) && (r == 0))
   solver_info.name = 'mosek';
   solver_info.termination = 'Normal termination';
-  x = vsdp.svec (obj, res.sol.itr.barx, 2);
+  x = to_vsdp_fmt(obj, res.sol.itr.barx);
   y = res.sol.itr.y;
-  z = vsdp.svec (obj, c - A*y, 1);
+  z = vsdp.svec (obj, obj.c - obj.At*y, 1);
   f_objective = [obj.c'*x; obj.b'*y];
   obj.add_solution (sol_type, x, y, z, f_objective, solver_info);
 end
@@ -81,8 +81,18 @@ end
 
 
 function [subi, subj, subk, subl, val] = to_mosek_fmt (x)
+% TO_MOSEK_FMT  Translate the VSDP quantities 'At' and 'c' to MOSEK format.
 %
+%   The output are four to five vectors of equal length representing data
+%   for the non-zero values of the VSDP quantities
 %
+%      subi - constraint index (only relevant for 'At')
+%      subj - cone       index
+%      subk - row        index
+%      subl - column     index
+%      val  - non-zero  values
+%
+%   See also vsdp.
 
 % Compute the lower triangular matrix in each cell.
 x = cellfun(@(x) tril (vsdp.smat([], x, 1)), x, 'UniformOutput', false);
@@ -91,16 +101,54 @@ x = cellfun(@(x) tril (vsdp.smat([], x, 1)), x, 'UniformOutput', false);
 [subk, subl, val] = cellfun(@find, x, 'UniformOutput', false);
 
 % Compute index vectors for the constraints (subi) and the cones (subj).
-[ii, jj] = meshgrid(1:size(x,2), 1:size(x,1));
-subi = cellfun(@(x, i) i * ones(size(x)), subk, num2cell(ii), ...
+[ii, jj] = meshgrid (1:size(x,2), 1:size(x,1));
+subi = cellfun (@(x, i) i * ones(size(x)), subk, num2cell(ii), ...
   'UniformOutput', false);
-subj = cellfun(@(x, j) j * ones(size(x)), subk, num2cell(jj), ...
+subj = cellfun (@(x, j) j * ones(size(x)), subk, num2cell(jj), ...
   'UniformOutput', false);
 
 % Vectorize quantities
-subi = vertcat(subi{:})'; % constraint index
-subj = vertcat(subj{:})'; % cone       index
-subk = vertcat(subk{:})'; % row        index
-subl = vertcat(subl{:})'; % column     index
-val  = vertcat(val{:})';  % values
+subi = vertcat (subi{:})'; % constraint index
+subj = vertcat (subj{:})'; % cone       index
+subk = vertcat (subk{:})'; % row        index
+subl = vertcat (subl{:})'; % column     index
+val  = vertcat (val{:})';  % non-zero  values
+end
+
+
+function x = to_vsdp_fmt (obj, x)
+% TO_VSDP_FMT  Translate the MOSEK solution 'X' to VSDP format.
+%
+%    The computations are similar to those in vsdp.svec.  The ordering of
+%    the entries inside the MOSEK solution for each SDP cone are shown
+%    below in Matrix 'J'.  Those have to be translated to the VSDP ordering
+%    shown in Matrix 'I' below.  See the following example for N = 5:
+%
+%            [ 1            ]         [ 1            ]
+%            [ 2  3         ]         [ 2  7         ]
+%        I = [ 4  5  6      ]     J = [ 3  8 13      ]
+%            [ 7  8  9 10   ]         [ 4  9 14 19   ]
+%            [11 12 13 14 15]         [ 5 10 15 20 25]
+%
+%    The difference to 'lidx' from vsdp.svec is that 'lidx' addresses the
+%    lower triangular part for a quadratic NxN matrix, whereas 'idx' below
+%    just addresses the vectorized
+%
+%   See also vsdp, vsdp.svec.
+
+% Index vector for sorting.
+idx = zeros(obj.n, 1);
+
+% For each SDP cone
+for j = 1:length(obj.K.s)
+  N = obj.K.s(j);
+  sidx = obj.K.idx.s(j,:);
+  I = zeros (N);
+  I(triu (true (N))) = 1:(N*(N+1)/2);
+  I = I';
+  I(I == 0) = [];
+  % Insert indices shifted by the cone offset.
+  idx(sidx(1):sidx(2),:) = I + sidx(1) - 1;
+end
+x = vsdp.svec (obj, x(idx), 2);
 end
