cd28414b0b20168987e1099c42ed4f05dd350fd4
Kai T. Ohlhus <k.ohlhus@gmail.com>
2019-03-25 16:56:28 +0100

    @vsdp/get_midpoint_problem_data(): Unprotect function.

---
+++ @vsdp/get_midpoint_problem_data.m
@@ -0,0 +1,20 @@
+function [At, b, c] = get_midpoint_problem_data (obj, sol_type)
+% GET_MIDPOINT_PROBLEM_DATA  Get midpoint data of the conic problem.
+%
+%   See also vsdp.
+%
+
+% Copyright 2004-2018 Christian Jansson (jansson@tuhh.de)
+
+[At, b, c] = deal (mid (obj.At), mid (obj.b), mid (obj.c));
+if ((nargin > 1) && ~strcmp (sol_type, "Approximate"))
+  % Return the perturbed midpoint problem just in case no approximate
+  % solution is requested.
+  if (~isempty (obj.perturbation.b))
+    b = b - obj.perturbation.b;
+  end
+  if (~isempty (obj.perturbation.c))
+    c = c - obj.perturbation.c;
+  end
+end
+end
