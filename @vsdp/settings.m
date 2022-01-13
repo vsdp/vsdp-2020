bca79e2b5376562179cf6e15619796c29c4f3fba
Kai T. Ohlhus <k.ohlhus@gmail.com>
2022-01-13 14:34:47 +0900

    @vsdp/settings.m: use readable YAML format for storage

--- @vsdp/settings.m
+++ @vsdp/settings.m
@@ -5,51 +5,125 @@ function val = settings (id, key, val)
 %
 %      vsdp.settings ('id', 'key', 'val');
 %      vsdp.settings ('vsdp', 'path', '/path/to/vsdp');
+%      vsdp.settings ('settings_file', 'path', '/path/to/vsdp_settings_file');
 %
 %   Load solver setting:
 %
 %      val = vsdp.settings ('id', 'key');
 %
 
-% Copyright 2004-2020 Christian Jansson (jansson@tuhh.de)
+% Copyright 2004-2022 Christian Jansson (jansson@tuhh.de)
 
 persistent vsdp_settings;  % In memory copy of VSDP settings file.
 persistent settings_file;
 
 narginchk (2, 3);
 
-% Assume first initialization.
-if (isempty (settings_file))
-  vsdp_path = fullfile (fileparts (which ('install_vsdp')));
-  settings_file = fullfile (vsdp_path, ...
-    sprintf ('vsdp_settings_%d.mat', sys_hash ()));
+vsdp_path = fullfile (fileparts (which ('install_vsdp')));
+
+% Set the settings_file string itself.
+if (strcmp (id, 'settings_file'))
+  if (nargin == 2)
+    val = settings_file;
+    return;
+  else
+    settings_file = val;
+    vsdp_settings = [];
+  end
+elseif (isempty (settings_file))  % Ensure default value.
+  settings_file = fullfile (vsdp_path, 'vsdp_settings.yaml');
+end
+
+% vsdp_settings initialization.
+if (isempty (vsdp_settings))
   if (exist (settings_file, 'file') == 2)
-    load (settings_file, 'vsdp_settings');
+    vsdp_settings = load_vsdp_settings (settings_file);
   else
     vsdp_settings.vsdp.path = vsdp_path;
-    save (settings_file, 'vsdp_settings', '-v7');
+    save_vsdp_settings (settings_file, vsdp_settings);
   end
 end
 
-switch (nargin)
-  case 2  % Load setting
-    val = [];
-    if (isfield (vsdp_settings, id) ...
-        && isfield (getfield (vsdp_settings, id), key))
-      val = getfield (getfield (vsdp_settings, id), key);
+% Finished when changing the settings_file.
+if (strcmp (id, 'settings_file'))
+  return;
+end
+
+% Load setting
+if (nargin == 2)
+  val = [];
+  if (isfield (vsdp_settings, id) ...
+      && isfield (getfield (vsdp_settings, id), key))
+    val = getfield (getfield (vsdp_settings, id), key);
+  end
+else  % Store setting
+  vsdp_settings = setfield (vsdp_settings, id, struct (key, val));
+  save_vsdp_settings (settings_file, vsdp_settings);
+end
+end
+
+
+function save_vsdp_settings (settings_file, vsdp_settings)
+  items = fieldnames (vsdp_settings);
+  str = sprintf ('%s\n', '---');
+  for i = 1:length (items)
+    item = vsdp_settings.(items{i});
+    keys = fieldnames (item);
+    str = sprintf ('%s%s\n', str, ['- "', items{i}, '":']);
+    for j = 1:length (keys)
+      key = keys{j};
+      val = item.(key);
+      str = sprintf ('%s%s\n', str, ['  - ', key, ': "', val, '"']);
     end
-  case 3  % Store setting
-    vsdp_settings = setfield (vsdp_settings, id, struct (key, val));
-    save (settings_file, 'vsdp_settings', '-v7');
-  otherwise
-    error ('VSDP:SOLVER:UTIL:settings', 'solver.util.settings: Bad usage.');
-end
+  end
+  fid = fopen (settings_file, 'w');
+  fprintf (fid, '%s', str);
+  fclose (fid);
 end
 
 
-function val = sys_hash ()
-% SYS_HASH  Compute a system unique hash value.
-%
+function vsdp_settings = load_vsdp_settings (settings_file)
+  vsdp_settings = struct ();
+  str = fileread (settings_file);
+  str = strsplit (str, {'\n', '\r'});
+  idx = cellfun (@isempty, str) | cellfun (@(s) strcmp (s, '---'), str);
+  str = str(~idx);
+  items = cellfun (@(s) (s(1) == '-'), str);
+  items = cumsum (items);  % 1 1 2 2 ...
+  for i = 1:max(items)
+    % [1,1] = - "vsdp":
+    % [1,2] =   - path: "/path/to/vsdp/"
+    % [1,3] =   - key: "val"
+    item = str(items == i);
+    id = get_quot_str_content (item{1});
+    item = item(2:end);
+    attrs = cell (1, length (item));
+    for j = 1:length (item)
+      attr = strsplit (item{j}, {':'});
+      attrs{2*j - 1} = get_quot_str_content (attr{1});
+      if (length (attr) > 1)
+        attrs{2*j} = get_quot_str_content (attr{2});
+      end
+    end
+    vsdp_settings = setfield (vsdp_settings, id, struct (attrs{:}));
+  end
+end
 
-val = sum (double ([computer(), '_vsdp_2020_', version()]));
+
+function str = get_quot_str_content (str)
+  rstr = regexp (str, '.*"(.*)"', 'tokens');
+  if (~ isempty (rstr))
+    str = rstr{1};
+    str = str{1};
+  else
+    % Heuristic to extract relevant string from YAML.
+    str = strtrim (str);
+    if (str(1) == '-')
+      str = str(2:end);
+    end
+    if (str(end) == ':')
+      str = str(1:end-1);
+    end
+    str = strtrim (str);
+  end
 end
--- install_vsdp.m
+++ install_vsdp.m
@@ -1,21 +1,30 @@
-function install_vsdp ()
+function install_vsdp (settings_file)
 % INSTALL_VSDP  Ensures VSDP to work properly.
+%
+%   For loading a custom settings file use:
+%
+%      install_vsdp ('my_vsdp_settings.yaml')
 %
 %   See also vsdp.
 %
 
-% Copyright 2004-2020 Christian Jansson (jansson@tuhh.de)
+% Copyright 2004-2022 Christian Jansson (jansson@tuhh.de)
 
 % Check whether path already exists and add path if necessary.
 vsdp_path = fullfile (fileparts (which ('install_vsdp')));
 addpath (vsdp_path);
 addpath (fullfile (vsdp_path, 'test'));
 if (exist ('OCTAVE_VERSION', 'builtin'))
+  warning ('off', 'Octave:shadowed-function', 'local');
   addpath (fullfile (vsdp_path, 'octave'));
 end
 path (path);  % Refresh path
 
 % Check for available solvers.
+if (nargin)
+  vsdp.settings ('settings_file', 'path', settings_file);
+end
+
 solver.registry.status ();
 
 end
